b1108e085cab41b717fbde92f3d2dcbac2116d49
Hamza Rarani <hamzararrani77@gmail.com>
2022-05-07 12:21:20 +0530

    fix: fixed some contrast issues

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,60 +1,61 @@
-import React from 'react';
-import { useDataProvider } from '../Providers/DataProvider';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import Card from '../Utils/Card';
-import { Header } from '../Styles/Header.styled';
+import React from "react";
+import { useDataProvider } from "../Providers/DataProvider";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Card from "../Utils/Card";
+import { Header } from "../Styles/Header.styled";
 
 const Home = () => {
-  //retrieving fetched data from provider
-  const { data, results, setNext } = useDataProvider();
+	//retrieving fetched data from provider
+	const { data, results, setNext } = useDataProvider();
 
-  return (
-    <>
-      <Header>
-        <img src='/img/randm.png' alt='rick and morty' className='img' />
-        <h1 className='title'>Rick & Morty</h1>
-      </Header>
-      {/* Using infinite scroll library of react to create infinite scrolling effect */}
-      <InfiniteScroll
-        dataLength={results.length}
-        next={
-          data && data.info.next
-            ? (window.onscroll = function () {
-                if (
-                  window.innerHeight + window.scrollY >=
-                  document.body.offsetHeight
-                ) {
-                  // If reached the end of the page code below runs
-                  setNext(data.info.next);
-                }
-              })
-            : null
-        }
-        hasMore={data && data.info.next}
-        loader={
-          <img
-            src='/img/spinner.gif'
-            style={{ width: '10rem', margin: 'auto', display: 'block' }}
-            alt='Loading...'
-          />
-          // Shows loader until data loads
-        }
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: '3rem',
-          padding: '3rem 5rem',
-        }}
-      >
-        {results.map((result, index) => (
-          // Rendering cards for every character recieved
-          <Card key={index} character={result} />
-        ))}
-      </InfiniteScroll>
-    </>
-  );
+	return (
+		<>
+			<Header>
+				<img src='/img/randm.webp' alt='rick and morty' className='img' />
+				<h1 className='title'>Rick & Morty</h1>
+			</Header>
+			{/* Using infinite scroll library of react to create infinite scrolling effect */}
+			<InfiniteScroll
+				dataLength={results.length}
+				next={
+					data && data.info.next
+						? (window.onscroll = function () {
+								if (
+									window.innerHeight + window.scrollY >=
+									document.body.offsetHeight
+								) {
+									// If reached the end of the page code below runs
+									setNext(data.info.next);
+								}
+								// eslint-disable-next-line no-mixed-spaces-and-tabs
+						  })
+						: null
+				}
+				hasMore={data && data.info.next}
+				loader={
+					<img
+						src='/img/spinner.gif'
+						style={{ width: "10rem", margin: "auto", display: "block" }}
+						alt='Loading...'
+					/>
+					// Shows loader until data loads
+				}
+				style={{
+					display: "flex",
+					flexWrap: "wrap",
+					alignItems: "center",
+					justifyContent: "center",
+					gap: "3rem",
+					padding: "3rem 5rem",
+				}}
+			>
+				{results.map((result, index) => (
+					// Rendering cards for every character recieved
+					<Card key={index} character={result} />
+				))}
+			</InfiniteScroll>
+		</>
+	);
 };
 
 export default Home;
--- src/Components/Utils/Card.jsx
+++ src/Components/Utils/Card.jsx
@@ -1,92 +1,93 @@
-import React from 'react';
-import { useDataProvider } from '../Providers/DataProvider';
-import PropTypes from 'prop-types';
+import React from "react";
+import { useDataProvider } from "../Providers/DataProvider";
+import PropTypes from "prop-types";
 import {
-  DisplayCard,
-  CardInfo,
-  NameGender,
-  Locations,
-  FeaturedIn,
-} from '../Styles/Card.styled';
+	DisplayCard,
+	CardInfo,
+	NameGender,
+	Locations,
+	FeaturedIn,
+} from "../Styles/Card.styled";
 
 const Card = ({
-  character: {
-    episode,
-    gender,
-    image,
-    location,
-    name,
-    origin,
-    species,
-    status,
-  },
+	character: {
+		episode,
+		gender,
+		image,
+		location,
+		name,
+		origin,
+		species,
+		status,
+	},
 }) => {
-  const { episodes } = useDataProvider(); // Getting episode list from data prvider
+	const { episodes } = useDataProvider(); // Getting episode list from data prvider
 
-  return (
-    <DisplayCard>
-      <section className='card-img'>
-        <img src={image} alt={name} className='img'></img>
-      </section>
-      <CardInfo>
-        <NameGender>
-          <h2 className='name'>{name}</h2>
-          <section className='other-detail'>
-            <p
-              style={{
-                color:
-                  status === 'Alive'
-                    ? '#00ff00'
-                    : status === 'Dead'
-                    ? '#ff0000'
-                    : '#D67D3E',
-              }}
-            >
-              {/* Updating the color of the status text according to status of the character */}
-              {status}
-            </p>
-            {'-'}
-            <p>
-              {species} {`(${gender})`}
-            </p>
-          </section>
-        </NameGender>
-        <Locations>
-          <div className='origin'>
-            <h3>Origin: </h3>
-            <p>{origin.name}</p>
-          </div>
-          <div className='current'>
-            <h3>Current: </h3>
-            <p>{location.name}</p>
-          </div>
-        </Locations>
-        <FeaturedIn>
-          <div className='first'>
-            <h3>First Appeared: </h3>
-            <p>
-              {episodes && episodes[parseInt(episode[0].split('/')[5] - 1)]}
-              {/* Getting the name of the episode based on the episode url in 0th index of episode array */}
-            </p>
-          </div>
-          <div className='last'>
-            <h3>Last Appeared: </h3>
-            <p>
-              {episodes &&
-                episodes[
-                  parseInt(episode[episode.length - 1].split('/')[5] - 1)
-                ]}
-              {/* Getting the name of the episode based on the episode url in last index of episode array */}
-            </p>
-          </div>
-        </FeaturedIn>
-      </CardInfo>
-    </DisplayCard>
-  );
+	return (
+		<DisplayCard>
+			<section className='card-img'>
+				<img src={image} alt={name} className='img'></img>
+			</section>
+			<CardInfo>
+				<NameGender>
+					<h2 className='name'>{name}</h2>
+					<section className='other-detail'>
+						<p
+							style={{
+								color:
+									status === "Alive"
+										? "#084594"
+										: status === "Dead"
+										? "#D82148"
+										: "#361500",
+								fontWeight: "600",
+							}}
+						>
+							{/* Updating the color of the status text according to status of the character */}
+							{status}
+						</p>
+						{"-"}
+						<p>
+							{species} {`(${gender})`}
+						</p>
+					</section>
+				</NameGender>
+				<Locations>
+					<div className='origin'>
+						<h3>Origin: </h3>
+						<p>{origin.name}</p>
+					</div>
+					<div className='current'>
+						<h3>Current: </h3>
+						<p>{location.name}</p>
+					</div>
+				</Locations>
+				<FeaturedIn>
+					<div className='first'>
+						<h3>First Appeared: </h3>
+						<p>
+							{episodes && episodes[parseInt(episode[0].split("/")[5] - 1)]}
+							{/* Getting the name of the episode based on the episode url in 0th index of episode array */}
+						</p>
+					</div>
+					<div className='last'>
+						<h3>Last Appeared: </h3>
+						<p>
+							{episodes &&
+								episodes[
+									parseInt(episode[episode.length - 1].split("/")[5] - 1)
+								]}
+							{/* Getting the name of the episode based on the episode url in last index of episode array */}
+						</p>
+					</div>
+				</FeaturedIn>
+			</CardInfo>
+		</DisplayCard>
+	);
 };
 
 Card.propTypes = {
-  character: PropTypes.object,
+	character: PropTypes.object,
 };
 
 export default Card;
